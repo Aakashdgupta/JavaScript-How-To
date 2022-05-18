9a2eeba6c647540ba800176eff877c3d89c4791d
Aakash Deep Gupta <gaakash1999@gmail.com>
2022-05-18 12:48:55 +0530

    Major improvement in quiz-app , now quiz class takes config object with html element id of que,opt1,opt2,opt3,opt4 anf handles event listiner by it self

--- projects/quizapp/main.js
+++ projects/quizapp/main.js
@@ -1,32 +1,78 @@
 class Quiz {
-  constructor(questions,config=null) {
+  constructor(questions, config = null) {
     this.questions = questions;
     this.score = 0;
     this.index = 0;
-      
+    this.config = config;
+
+    this.qEl;
+    this.opt1El;
+    this.opt2El;
+    this.opt3El;
+    this.opt4El;
+
+    if (this.config !== null) {
+      console.log("  not empty");
+      let ctx = this;
+
+      if (ctx.config) {
+          ctx.qEl = document.getElementById(ctx.config.que);
+          console.log(ctx.config)
+
+          ctx.opt1El = document.getElementById(ctx.config.opt1);
+          console.log(ctx.opt1El)
+
+        ctx.opt1El.addEventListener("click", (e) => {
+          ctx.checkans(ctx.questions[ctx.index][e.target.id]);
+        });
+          
+          
+        ctx.opt2El = document.getElementById(this.config.opt2);
+        console.log(ctx.opt2El);
+        ctx.opt2El.addEventListener("click", (e) => {
+          ctx.checkans(ctx.questions[ctx.index][e.target.id]);
+        });
+          
+          
+          
+        ctx.opt3El = document.getElementById(ctx.config.opt3);
+
+        ctx.opt3El.addEventListener("click", (e) => {
+          ctx.checkans(ctx.questions[ctx.index][e.target.id]);
+        });
+      }
+
+      ctx.opt4El = document.getElementById(ctx.config.opt4);
+
+      ctx.opt4El.addEventListener("click", (e) => {
+        ctx.checkans(ctx.questions[ctx.index][e.target.id]);
+      });
+
+    } else {
+      console.log("empty");
+    }
   }
 
   updateQuestions() {
-    q.innerHTML = this.questions[this.index].question;
-    opt1.innerHTML = this.questions[this.index].opt1;
-    opt2.innerHTML = this.questions[this.index].opt2;
-    opt3.innerHTML = this.questions[this.index].opt3;
-    opt4.innerHTML = this.questions[this.index].opt4;
+    this.qEl.innerHTML = this.questions[this.index].question;
+    this.opt1El.innerHTML = this.questions[this.index].opt1;
+    this.opt2El.innerHTML = this.questions[this.index].opt2;
+    this.opt3El.innerHTML = this.questions[this.index].opt3;
+    this.opt4El.innerHTML = this.questions[this.index].opt4;
+
   }
 
   checkans(opt) {
-      if (opt === this.questions[this.index].ans) {
-        this.score++;        
-      }
+    if (opt === this.questions[this.index].ans) {
+      this.score++;
+    }
     this.index++;
-      if (this.index < this.questions.length) {
-        this.updateQuestions();
-      } else {
-        alert(this.score);
-        }
-    // console.log(" option : " + opt);
-    // console.log(" score : " + this.score);
-    // console.log(" index : " + this.index);
+    if (this.index < this.questions.length) {
+      this.updateQuestions();
+    } else {
+      alert(this.score);
+    }
+
   }
 }
 
@@ -65,31 +111,15 @@ let questions = [
   },
 ];
 
-//elements
-let q = document.getElementById("que");
-let opt1 = document.getElementById("opt1");
-let opt2 = document.getElementById("opt2");
-let opt3 = document.getElementById("opt3");
-let opt4 = document.getElementById("opt4");
+
 let n = 0;
 score = 0;
 
-quiz = new Quiz(questions);
+quiz = new Quiz(questions, {
+  que: "que",
+  opt1: "opt1",
+  opt2: "opt2",
+  opt3: "opt3",
+  opt4: "opt4",
+});
 quiz.updateQuestions();
-
-opt1.addEventListener("click", (e) => {
-  quiz.checkans(quiz.questions[quiz.index][e.target.id]);
-});
-
-opt2.addEventListener("click", (e) => {
-  quiz.checkans(quiz.questions[quiz.index][e.target.id]);
-});
-
-opt3.addEventListener("click", (e) => {
-  quiz.checkans(quiz.questions[quiz.index][e.target.id]);
-});
-
-opt4.addEventListener("click", (e) => {
-  quiz.checkans(quiz.questions[quiz.index][e.target.id]);
-});
-
